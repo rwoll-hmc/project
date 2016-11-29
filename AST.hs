24da5f8581a5532b6f6453757dd230cfbc671c5a
rwoll <ross.wollman@gmail.com>
2016-11-28 23:57:35 -0800

    Add "ugly" pretty printer
    The pretty printer is not syntax compliant, but it's an easier way
    to see what is going on.

--- AST.hs
+++ AST.hs
@@ -3,9 +3,18 @@ data Command = Go | Stby | Warn deriving (Eq, Show)
 data Character = Character String deriving (Eq, Show)
 data Action = Enter | Exit deriving (Eq, Show)
 data Department = Department String deriving (Eq, Show)
-data CueSheet = CueSheet { characters :: [Character], departments :: [Department], acts :: [Act] } deriving (Eq, Show)
+data CueSheet = CueSheet { characters :: [Character], departments :: [Department], acts :: [Act] } deriving (Eq)
 data Cue = Cue { department :: Department, number :: Int, command :: Command } deriving (Eq, Show)
 data CueGroup = CueGroup Marker [Cue] deriving (Eq, Show)
 data Marker = Visual Character Action | Line Character String deriving (Eq, Show)
 data Scene = Scene Int [CueGroup] deriving (Eq, Show)
 data Act = Act Int [Scene] deriving (Eq, Show)
+
+instance Show CueSheet where
+  show (CueSheet cs ds as) = (unlines $ map show cs) ++
+                             (unlines $ map show ds) ++
+                             (unlines $ map showAct as) where
+
+    showAct (Act i ss) = "Act " ++ show i ++ "\n" ++ (unlines $ map showScene ss)
+    showScene (Scene i gs) = "  Scene " ++ show i ++ "\n" ++ (unlines $ map showCG gs)
+    showCG (CueGroup m cs) = "    " ++ show m ++ "\n" ++ (unlines $ map (("      " ++) . show) cs)
