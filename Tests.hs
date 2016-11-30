cc4bbd1b1bbed39dd6b375c2305a314bf10089bc
rwoll <ross.wollman@gmail.com>
2016-11-30 15:35:39 -0800

    Add tests for fuzzy matcher

--- Tests.hs
+++ Tests.hs
@@ -6,13 +6,17 @@ import Test.Tasty.HUnit
 import qualified Text.Parsec as Parsec
 import qualified AST
 import qualified Parser
+import Interp (fuzzyMatch)
 
 main = defaultMain tests
 
 pr p s = Parsec.parse p "Sample Input" s
 
 tests :: TestTree
-tests = testGroup "Parser Tests"
+tests = testGroup "Unit Tests" [parserTests, interpTests]
+
+parserTests :: TestTree
+parserTests = testGroup "Parser Tests"
   [ characterTests,
     deptTests,
     cmdTests,
@@ -58,6 +62,40 @@ cueNumber = testGroup "Formatted Cue Number" $
     ("{47}", 47, Nothing)
   ]
 
+interpTests :: TestTree
+interpTests = testGroup "Interpreter Tests" [fuzzyMatchTests]
+
+fuzzyMatchTests :: TestTree
+fuzzyMatchTests = testGroup "Fuzzy Match Tests"
+  [ testGroup "Visual" $
+      [
+        testCase "Exactly Equal" $
+          fuzzyMatch (AST.Visual (AST.Character "CECIL") AST.Enter (Just 0)) (AST.Visual (AST.Character "CECIL") AST.Enter (Just 0)) @?= True,
+        testCase "Unspecified Index" $
+          fuzzyMatch (AST.Visual (AST.Character "CECIL") AST.Enter (Just 0)) (AST.Visual (AST.Character "CECIL") AST.Enter Nothing) @?= True,
+        testCase "Different Characters (should fail)" $
+          fuzzyMatch (AST.Visual (AST.Character "CECIL") AST.Enter (Just 0)) (AST.Visual (AST.Character "ATHENA") AST.Enter (Just 0)) @?= False,
+        testCase "Different Indexes (should fail)" $
+          fuzzyMatch (AST.Visual (AST.Character "CECIL") AST.Enter (Just 1)) (AST.Visual (AST.Character "CECIL") AST.Enter (Just 0)) @?= False
+      ],
+    testGroup "Line" $
+      [
+        testCase "Exactly Equal" $
+          fuzzyMatch (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 0)) (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 0)) @?= True,
+        testCase "Substring Match Equal with Index" $
+          fuzzyMatch (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 0)) (AST.Line (AST.Character "CECIL") "Wo" (Just 0)) @?= True,
+        testCase "Equal with Unspecified Index" $
+          fuzzyMatch (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 0)) (AST.Line (AST.Character "CECIL") "Hello, World!" Nothing) @?= True,
+        testCase "Substring Match with Unspecified Index" $
+          fuzzyMatch (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 0)) (AST.Line (AST.Character "CECIL") "Wo" Nothing) @?= True,
+        testCase "Equal Speech, Differing Indexes (should fail)" $
+          fuzzyMatch (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 0)) (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 1)) @?= False,
+        testCase "Substring Match with Differing Indexes (should fail)" $
+          fuzzyMatch (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 0)) (AST.Line (AST.Character "CECIL") "Wo" (Just 1)) @?= False,
+        testCase "Without Substring or Index (should fail)" $
+          fuzzyMatch (AST.Line (AST.Character "CECIL") "Hello, World!" (Just 0)) (AST.Line (AST.Character "CECIL") "no match" Nothing) @?= False
+      ]
+  ]
 reflectTests f = map (\(a, e, comment) -> testCase (genLabel comment a) $ f a @?= Right e) where
   genLabel (Just c) _ = c
   genLabel Nothing a = a
