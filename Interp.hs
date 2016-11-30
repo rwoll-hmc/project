4444cc0996811afc5c058bdf362a147f6b2be95e
rwoll <ross.wollman@gmail.com>
2016-11-30 15:35:17 -0800

    Add cue fuzzy matcher logic

---
+++ Interp.hs
@@ -0,0 +1,16 @@
+module Interp where
+
+import AST (Marker(..))
+import qualified Utils
+
+fuzzyMatch :: Marker -- ^ Possible match
+           -> Marker -- ^ Search Criteria
+           -> Bool   -- ^ True if fuzzy match, False otherwise.
+fuzzyMatch (Visual _ _ _) (Line _ _ _) = False
+fuzzyMatch (Line _ _ _) (Visual _ _ _) = False
+fuzzyMatch (Line c0 l0 (Just i0)) (Line c1 l1 Nothing) = c0 == c1 && Utils.substring l1 l0
+fuzzyMatch (Line c0 l0 (Just i0)) (Line c1 l1 (Just i1)) = c0 == c1 && i0 == i1 && Utils.substring l1 l0
+fuzzyMatch (Visual c0 a0 (Just i0)) (Visual c1 a1 Nothing) = c0 == c1 && a0 == a1
+fuzzyMatch v0@(Visual _ _ (Just _)) v1@(Visual _ _ (Just _)) = v0 == v1
+fuzzyMatch (Line _ _ Nothing) _ = error("TODO: Reimplement the data structure")
+fuzzyMatch (Visual _ _ Nothing) _ = error("TODO: Reimplement the data structure")
