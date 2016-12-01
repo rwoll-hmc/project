42e61035af9424ae44dce2a204506b4b5015a8ad
rwoll <ross.wollman@gmail.com>
2016-11-30 16:30:14 -0800

    Add scene level ambiguity checker

--- Interp.hs
+++ Interp.hs
@@ -1,6 +1,6 @@
 module Interp where
 
-import AST (Marker(..))
+import AST (Marker(..), PromptScene(..), CueGroup(..), pMarkers, pMarker)
 import qualified Utils
 
 fuzzyMatch :: Marker -- ^ Possible match
@@ -14,3 +14,8 @@ fuzzyMatch (Visual c0 a0 (Just i0)) (Visual c1 a1 Nothing) = c0 == c1 && a0 == a
 fuzzyMatch v0@(Visual _ _ (Just _)) v1@(Visual _ _ (Just _)) = v0 == v1
 fuzzyMatch (Line _ _ Nothing) _ = error("TODO: Reimplement the data structure")
 fuzzyMatch (Visual _ _ Nothing) _ = error("TODO: Reimplement the data structure")
+
+isAmbiguous :: PromptScene -> CueGroup -> Bool
+isAmbiguous _ (CueGroup (Visual _ _ (Just _)) _) = False
+isAmbiguous _ (CueGroup (Line _ _ (Just _)) _) = False
+isAmbiguous (PromptScene _ pms) (CueGroup m _) = length (filter (\pm -> fuzzyMatch (pMarker pm) m) pms) > 1
