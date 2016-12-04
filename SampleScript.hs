bd86fe33a50e705f45dd7061872ee3f1440b59ca
rwoll <ross.wollman@gmail.com>
2016-12-04 14:46:47 -0800

    Add documentation for the SampleScript
    * Reports 100% coverage with Haddock.
    * Reports 100% clean formatting with hfmt.

--- SampleScript.hs
+++ SampleScript.hs
@@ -1,9 +1,11 @@
+-- | A sample script to user for presentations/examples.
 module SampleScript where
 
 import           AST
 import qualified Data.Map.Strict as Map
 import qualified Data.Set        as Set
 
+-- | A sample script.
 theScript :: PromptScript
 theScript = PromptScript
               "DSLs: A Play"
@@ -16,11 +18,9 @@ theScript = PromptScript
                  ])
               (Set.fromList [Department "LX", Department "SD"])
               (Map.fromList [(0, a)])
-
-a = PromptAct $ Map.fromList [(0,
-  PromptScene $ Map.fromList
-    [ (0, PromptMarker (Visual (Character "CECIL") Enter Nothing) [])
-    , (1, PromptMarker (Line (Character "CECIL") "Hello, world!" Nothing) [])
-    , (2, PromptMarker (Line (Character "CECIL") "Hello, world!" Nothing) [])
-    ]
-    )]
+  where
+    a = PromptAct $ Map.fromList [(0, PromptScene $ Map.fromList lines')]
+    lines' = [ (0, PromptMarker (Visual (Character "CECIL") Enter Nothing) [])
+             , (1, PromptMarker (Line (Character "CECIL") "Hello, world!" Nothing) [])
+             , (2, PromptMarker (Line (Character "CECIL") "Hello, world!" Nothing) [])
+             ]
