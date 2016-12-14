9b01722b4ec9c1748ec9e61ad5c928a1bcce27e0
rwoll <ross.wollman@gmail.com>
2016-12-13 18:55:58 -0800

    Add UI for web IDE

--- src/Server.hs
+++ src/Server.hs
@@ -11,16 +11,28 @@ import qualified Text.Parsec as Parsec
 import Parser (cueSheet)
 import Interp
 import Data.String.Conversions (cs)
+import Network.Wai.Middleware.Static
+import Errors
+import Data.Aeson (ToJSON)
+import GHC.Generics (Generic)
+
 
 main = do
     putStrLn "Starting Server..."
     scotty 3000 $ do
+        middleware $ staticPolicy (noDots >-> addBase "build")
 
         post "/process" $ do
           c <- body
           either handleErrors handleParsed (Parsec.parse cueSheet "i/o" $ cs c)
 
-handleErrors e = error $ show e
+handleErrors e = do
+  json $ ErrorResponse $ cs $ prettyError e
+
 handleParsed p = either handleErrors handleCompiled (transpile p)
 handleCompiled c = either handleErrors handleSafe (checkUnkownCharacters theScript c) where
   handleSafe _ = either handleErrors json (eval theScript c)
+
+data ErrorResponse = ErrorResponse { errors :: String } deriving (Eq, Show, Generic)
+
+instance ToJSON ErrorResponse
